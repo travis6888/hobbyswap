04a181b9e098fcd6808289b21061c256f0903cb2
Travis <travis6888@yahoo.com>
2014-09-26 21:57:46 -0700

    finished redo post page

--- hobbyswap/static/js/bootstrap-datepicker.js
+++ hobbyswap/static/js/bootstrap-datepicker.js
@@ -1,22 +1,16 @@
-$(document).ready( function(){
+$(document).ready(function () {
 
-$('#sandbox-container .input-group.date').datepicker({
-    orientation: "bottom auto",
-    autoclose: true,
-    beforeShowDay: function (date){
-      if (date.getMonth() == (new Date()).getMonth())
-        switch (date.getDate()){
-          case 4:
-            return {
-              tooltip: 'Example tooltip',
-              classes: 'active'
-            };
-          case 8:
-            return false;
-          case 12:
-            return "green";
-        }
-    }
+    $('.BtnFirst').on('click', function () {
+        $('.firstPart').toggle('slow');
+        $('.secondPart').toggle('slow');
+
+    });
+    $('.BtnLast').on('click', function () {
+
+        $('.secondPart').toggle('slow');
+
+        $('.lastPart').toggle('slow');
+    });
+
+
 });
-    })
-;
